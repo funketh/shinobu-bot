5e62bbca6a827e8c5b77228c3a8eefd9c9d4b211
funketh <theodor.k.funke@gmail.com>
2019-03-09 23:31:23 +0100

    Edited log file path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from CONSTANTS import DB_PATH
 from shinobu import Shinobu
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='example.log', level=logging.DEBUG)
+    logging.basicConfig(filename='logs/shinobu.log', level=logging.DEBUG)
     with sqlite3.connect(DB_PATH) as db:
         prefix, token = db.execute('SELECT command_prefix, token FROM config').fetchone()
     bot = Shinobu(command_prefix=prefix)
