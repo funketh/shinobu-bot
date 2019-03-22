2dd2cc30a89e6a91170e7b06afa40a158a70aa8d
funketh <theodor.k.funke@gmail.com>
2019-03-22 23:38:21 +0100

    add warnings and url attribute

--- utils/myanimelist_scraper.py
+++ utils/myanimelist_scraper.py
@@ -1,4 +1,5 @@
 import aiohttp
+import logging
 import re
 
 
@@ -12,55 +13,59 @@ UNDEFINED = object()
 
 
 class _BaseScraper:
-    def __init__(self, page: str):
+    def __init__(self, page: str, url: str):
         self.page = page
+        self.url = url
 
     @classmethod
     async def from_url(cls, url):
-        return cls(await get_page(url))
+        return cls(await get_page(url), url)
 
 
 class Anime(_BaseScraper):
-    def __init__(self, page: str):
-        super().__init__(page)
+    def __init__(self, page: str, url: str):
+        super().__init__(page, url)
         self._title, self._status, self._thumbnail, self._score = (UNDEFINED,) * 4
 
     @classmethod
     async def from_id(cls, id_):
-        return await cls.from_url(f"http://myanimelist.net/anime/{id_}")
+        return await cls.from_url(f"https://myanimelist.net/anime/{id_}")
 
     @property
     def title(self):
         if self._title is UNDEFINED:
-            self._title = re.search(r'(?<=<span itemprop="name">).+?(?=</span>)', self.page).group()
+            matches = re.findall(r'(?<=<span itemprop="name">).+?(?=</span>)', self.page)
+            if len(matches) > 1:
+                logging.warning(f'Found multiple titles for the anime {self.score}: {matches}')
+            self._title = matches[0] if matches else None
         return self._title
 
     @property
     def thumbnail(self):
         if self._thumbnail is UNDEFINED:
-            match = re.search(r'(?<=<img src=").+?(?=" alt=".+?" class="ac" itemprop="image">)', self.page)
-            self._thumbnail = match.group() if match else None
+            matches = re.findall(r'(?<=<img src=").+?(?=" alt=".+?" class="ac" itemprop="image">)', self.page)
+            if len(matches) > 1:
+                logging.warning(f'Found multiple scores for the anime {self.score}: {matches}')
+            self._thumbnail = matches[0] if matches else None
         return self._thumbnail
 
     @property
     def score(self):
         if self._score is UNDEFINED:
-            match = re.search(r'title="indicates a weighted score\. '
-                              r'Please note that \'Not yet aired\' titles are excluded\.">\s*?'
-                              r'(?P<m>\d\.\d\d)\s*?</div>', self.page)
-            self._score = float(match.group('m')) if match else None
+            matches = re.findall(r'title="indicates a weighted score\. '
+                                 r'Please note that \'Not yet aired\' titles are excluded\.">\s*?'
+                                 r'(<m>\d\.\d\d)\s*?</div>', self.page)
+            if len(matches) > 1:
+                logging.warning(f'Found multiple scores for the anime {self.score}: {matches}')
+            self._score = float(matches[0]) if matches else None
         return self._score
 
     @property
     def status(self):
         if self._status is UNDEFINED:
-            match = re.search(r'<div>\s*?<span class="dark_text">Status:</span>\s*?'
-                              r'(?P<m>\S.+?)[\r\n]+\s*?</div>', self.page)
-            self._status = match.group('m') if match else None
+            matches = re.findall(r'<div>\s*?<span class="dark_text">Status:</span>\s*?'
+                                 r'(\S.+?)[\r\n]+\s*?</div>', self.page)
+            if len(matches) > 1:
+                logging.warning(f'Found multiple statuses for the anime {self.score}: {matches}')
+            self._status = matches[0] if matches else None
         return self._status
-
-
-if __name__ == '__main__':
-    import asyncio
-
-    ani = asyncio.run(Anime.from_id(39265))
