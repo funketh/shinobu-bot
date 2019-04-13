5f3a815f741db6583e71ee0a29d28f5a095dbb6b
funketh <theodor.k.funke@gmail.com>
2019-04-13 17:33:02 +0200

    move config and token out of the database

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,3 +1,2 @@
 DB_PATH = 'data/shinobu.db'
-# TODO move to db
-FILE_ROOT = 'files'
+CMD_PREFIX = '.'
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import sqlite3
-
 import logging
 
-from CONSTANTS import DB_PATH
+import CONSTANTS
 from shinobu import Shinobu
 
 if __name__ == '__main__':
     logging.basicConfig(filename='logs/shinobu.log', level=logging.DEBUG)
-    with sqlite3.connect(DB_PATH) as db:
-        prefix, token = db.execute('SELECT command_prefix, token FROM config').fetchone()
-    bot = Shinobu(command_prefix=prefix)
+    bot = Shinobu(command_prefix=CONSTANTS.CMD_PREFIX)
+    with open('TOKEN') as f:
+        token = f.readline().strip()
     bot.run(token)
