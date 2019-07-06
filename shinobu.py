b2a003b07f42a355a2916b5b9617fca3caba5fe9
funketh <theodor.k.funke@gmail.com>
2019-07-06 20:45:22 +0200

    make extensions_modules a method

--- shinobu.py
+++ shinobu.py
@@ -1,18 +1,19 @@
+import discord
 import logging
 import os
 from discord.ext.commands import Bot, ExtensionNotLoaded
 
 
-async def extension_modules():
-    ext = 'extensions'
-    for file_name in os.listdir(ext):
-        if file_name.endswith('.py'):
-            yield f'{ext}.{file_name[:-3]}'
-
-
 class Shinobu(Bot):
+    @staticmethod
+    async def _extension_modules():
+        ext = 'extensions'
+        for file_name in os.listdir(ext):
+            if file_name.endswith('.py'):
+                yield f'{ext}.{file_name[:-3]}'
+
     async def reload_all_extensions(self):
-        async for ext in extension_modules():
+        async for ext in self._extension_modules():
             try:
                 self.reload_extension(ext)
             except ExtensionNotLoaded:
