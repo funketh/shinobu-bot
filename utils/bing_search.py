9938b7600dc380f82723dc43906832554feda2d9
funketh <theodor.k.funke@gmail.com>
2019-07-07 18:30:49 +0200

    switch from using bs4 to re

--- utils/bing_search.py
+++ utils/bing_search.py
@@ -6,7 +6,6 @@ from typing.re import Match
 from urllib.parse import quote_plus
 
 import aiohttp
-from bs4 import BeautifulSoup
 
 _BASE_URL = 'https://www.bing.com'
 
@@ -14,22 +13,21 @@ _BASE_URL = 'https://www.bing.com'
 async def search(query, session: aiohttp.ClientSession, delay=0.5):
     url = _BASE_URL + f'/search?q={quote_plus(query)}'
     async for page in result_pages(session, url):
-        for result in page.find(id='b_results').find_all(class_='b_algo'):
-            yield result.find('a')['href']
+        for match in re.finditer(r'<h2>.+?href="(.+?)"', page):
+            yield match.group(1)
         await asyncio.sleep(delay)
 
 
-async def result_pages(session: aiohttp.ClientSession, url):
+async def result_pages(session: aiohttp.ClientSession, url: str):
     while True:
         async with session.get(url) as resp:
-            page = BeautifulSoup(await resp.text(), 'html.parser')
-        if page.find(id='b_results'):
-            yield page
+            page = await resp.text()
+        yield page
         # Next page of search results
-        try:
-            url = _BASE_URL + page.find(class_='sb_pagN sb_pagN_bp b_widePag sb_bp ')['href']
-        except TypeError:
+        next_url_match = re.search(r'title="Next page" href="(.+?)"', page)
+        if not next_url_match:
             return
+        url = next_url_match.group(1)
 
 
 async def first_match(regex: AnyStr, strings: AsyncIterable) -> Optional[Match]:
