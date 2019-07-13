37a082e9a1932d4d5e16a788d1bd2b2e8887debd
funketh <theodor.k.funke@gmail.com>
2019-07-13 22:46:47 +0200

    add birthday presents

--- extensions/economy.py
+++ extensions/economy.py
@@ -1,4 +1,5 @@
 import discord
+import logging
 from discord.ext import commands, tasks
 
 from CONSTANTS import CURRENCY
@@ -6,11 +7,15 @@ from shinobu import Shinobu
 from utils import database
 from utils.messages import inform
 
+logger = logging.getLogger(__name__)
+
 
 class Economy(commands.Cog):
     def __init__(self, bot: Shinobu):
         self.bot = bot
         self.passive_income.start()
+        self.birthday.start()
+        await self.birthday.coro()
 
     def cog_unload(self):
         self.passive_income.cancel()
@@ -20,6 +25,17 @@ class Economy(commands.Cog):
         db = database.connect()
         with db:
             db.execute('UPDATE user SET income=MIN(income+1, 25)')
+
+    @tasks.loop(hours=24)
+    async def birthday(self):
+        db = database.connect()
+        with db:
+            for user in db.execute("SELECT * FROM user WHERE birthday == DATE('now', 'localtime')").fetchall():
+                db.execute('UPDATE user SET balance=balance+100, birthday=? WHERE id=?',
+                           [add_years(user['birthday'], 1), user['id']])
+                await self.bot.get_user(user['id']).send(
+                    f'🎉🎉🎉 Happy Birthday! 🎉🎉🎉\nAs a present, you get 100{CURRENCY}!')
+                logger.info(f'gifted 100 to {user["name"]} as a birthday present!')
 
     @commands.command(name='withdraw', aliases=['w'])
     async def withdraw_income_cmd(self, ctx: commands.Context):
@@ -37,6 +53,10 @@ class Economy(commands.Cog):
         db = database.connect()
         balance = db.execute('SELECT balance FROM user WHERE id=?', [user.id]).fetchone()['balance']
         await inform(ctx, f'{user.mention}\'s balance: {balance}{CURRENCY}')
+
+
+def add_years(date_: str, amount: int) -> str:
+    return str(int(date_[:4]) + amount) + date_[4:]
 
 
 def setup(bot: Shinobu):
