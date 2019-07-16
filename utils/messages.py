c18b536d4d127dbeacf729e1961dd5c1ddd90aed
funketh <theodor.k.funke@gmail.com>
2019-07-16 17:15:48 +0200

    add confirmation reaction helper

--- utils/messages.py
+++ utils/messages.py
@@ -1,7 +1,8 @@
-from functools import partial
+import asyncio
 
 import discord
-from typing import Optional
+from functools import partial
+from typing import Optional, List
 
 
 async def send_embed(color, messageable: discord.abc.Messageable,
@@ -12,4 +13,37 @@ async def send_embed(color, messageable: discord.abc.Messageable,
         color=color, **kwargs)
     )
 
+
 inform = partial(send_embed, discord.Color.green())
+error = partial(send_embed, discord.Color.red())
+
+
+async def confirm(bot: discord.Client, message: discord.Message, user: discord.User,
+                  yes: str = '👍', no: str = '👎', timeout: int = 60) -> bool:
+    def check(r, u):
+        return r.message == message and u == user and str(r.emoji) in (yes, no)
+    try:
+        reaction, _ = await bot.wait_for('reaction_add', timeout=timeout, check=check)
+    except asyncio.TimeoutError:
+        return False
+    return reaction.emoji == yes
+
+
+async def confirm_multiuser(bot: discord.Client, message: discord.Message, users: List[discord.User],
+                            yes: str = '👍', no: str = '👎', timeout: int = 60) -> bool:
+    def check(r, u):
+        return r.message == message and u in users and str(r.emoji) in (yes, no)
+
+    while len(users) > 0:
+        try:
+            reaction, _ = await bot.wait_for('reaction_add', timeout=timeout, check=check)
+        except asyncio.TimeoutError:
+            return False
+        if reaction.emoji == no:
+            return False
+        for u in await reaction.users().flatten():
+            try:
+                users.remove(u)
+            except ValueError:
+                pass
+    return True
