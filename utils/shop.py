e5bfd5564c563ba4334e233fb0ad1319651c5a91
funketh <theodor.k.funke@gmail.com>
2019-07-16 17:16:12 +0200

    formatting

--- utils/shop.py
+++ utils/shop.py
@@ -8,9 +8,12 @@ from typing import Tuple, Optional, NamedTuple
 CURRENT_PREDICATE = "((pack.start_date <= CURRENT_DATE) " \
                     " AND (pack.end_date IS NULL OR pack.end_date >= CURRENT_DATE))"
 
+
 class NotEnoughMoney(BaseException): pass
 
+
 class UnknownPackName(BaseException): pass
+
 
 class Receipt(NamedTuple):
     character: sqlite3.Row
@@ -18,6 +21,7 @@ class Receipt(NamedTuple):
     old_rarity: Optional[sqlite3.Row]
     pack: sqlite3.Row
     refund: Optional[int]
+
 
 async def buy_pack(db: DB, user_id: int, pack_name: str) -> Receipt:
     with db:
@@ -28,8 +32,8 @@ async def buy_pack(db: DB, user_id: int, pack_name: str) -> Receipt:
         old_rarity = give_waifu(db, user_id, char['id'], rarity['value'])
         gets_refund = old_rarity is not None and rarity['value'] <= old_rarity['value']
         refund_amount = refund(db, user_id, rarity['value'], pack['id']) if gets_refund else None
-        return Receipt(character=char, rarity=rarity, old_rarity=old_rarity,
-                       pack=pack, refund=refund_amount)
+        return Receipt(character=char, rarity=rarity, old_rarity=old_rarity, pack=pack, refund=refund_amount)
+
 
 def add_money(db: DB, user_id: int, amount: int):
     try:
@@ -37,17 +41,21 @@ def add_money(db: DB, user_id: int, amount: int):
     except sqlite3.IntegrityError:
         raise NotEnoughMoney
 
+
 def pick_from_pack(db: DB, pack_id: int) -> Tuple[sqlite3.Row, sqlite3.Row]:
     rarity = pick_rarity(db)
     char = pick_character(db, pack_id, rarity['value'])
     return char, rarity
 
+
 def random_choice(*args, **kwargs):
     return (random.choices(*args, **kwargs))[0]
+
 
 def pick_rarity(db: DB) -> sqlite3.Row:
     rarities = db.execute('SELECT * FROM rarity').fetchall()
     return random_choice(rarities, weights=(r['weight'] for r in rarities))
+
 
 def pick_character(db: DB, pack_id: int, rarity_val: int) -> sqlite3.Row:
     chars = db.execute("""
@@ -61,6 +69,7 @@ def pick_character(db: DB, pack_id: int, rarity_val: int) -> sqlite3.Row:
     """, [pack_id, rarity_val]).fetchall()
     return random_choice(chars, weights=(v['weight'] for v in chars))
 
+
 def give_waifu(db: DB, user_id: int, char_id: int, new_rarity_val: int) -> Optional[sqlite3.Row]:
     old_rarity = db.execute('''SELECT rarity.* FROM waifu
                                JOIN rarity ON rarity.value=waifu.rarity
@@ -70,6 +79,7 @@ def give_waifu(db: DB, user_id: int, char_id: int, new_rarity_val: int) -> Optio
         db.execute('REPLACE INTO waifu(user, character, rarity) VALUES(?, ?, ?)',
                    [user_id, char_id, new_rarity_val])
     return old_rarity
+
 
 def refund(db: DB, user_id: int, rarity_val: int, pack_id: int) -> int:
     amount = math.ceil(db.execute("""
