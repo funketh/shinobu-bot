e5fd57f6dad6fa604a98fc2f71033c2f73860d6e
funketh <theodor.k.funke@gmail.com>
2019-09-21 01:12:26 +0200

    fix

--- utils/formatting.py
+++ utils/formatting.py
@@ -1,8 +1,15 @@
+MAX_MESSAGE_LENGTH = 2000
+
+
 def paginate(text: str, prefix: str = '', suffix: str = ''):
+    max_content_len = MAX_MESSAGE_LENGTH - (len(prefix) + len(suffix))
     i = 0
     while i < len(text):
         old_i = i
-        max_next_i = max(old_i + 1024, len(text))
-        after_newline = text[i:max_next_i].rfind('\n')
-        i = max_next_i if after_newline < old_i else after_newline
+        max_next_i = old_i + max_content_len
+        if max_next_i > len(text):
+            i = len(text)
+        else:
+            after_newline = text[i:max_next_i].rfind('\n')
+            i = max_next_i if after_newline < old_i else after_newline
         yield prefix + text[old_i:i] + suffix
