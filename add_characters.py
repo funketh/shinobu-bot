f7c21838a8498bbc71834a17046d56e376a7e6b2
funketh <theodor.k.funke@gmail.com>
2020-01-02 23:35:18 +0100

    update script

--- add_characters.py
+++ add_characters.py
@@ -10,40 +10,26 @@ if __name__ == '__main__':
     with open(sys.argv[1], newline='') as tsvfile:
         new_chars = list(csv.DictReader(tsvfile, delimiter='\t'))
     db = database.connect()
+    id_key = [k for k in new_chars[0].keys() if k.startswith('id')][0]
     tmp = []
     for char in new_chars:
         char = {k: v.strip() for k, v in char.items()}
         try:
-            char['id'] = int(char['id'])
+            char['id'] = int(char[id_key])
+            del char[id_key]
         except ValueError:
             print(f'ERRONEOUS ENTRY: {char["name"]}')
         else:
             char['rarity'] = int(char['rarity'])
-            char['url'] = char['url'] or None
+            char['image_url'] = char['image_url'] or None
             tmp.append(char)
     new_chars = tmp
 
-    # Name conflicts
-    new_names = []
-    for c in new_chars:
-        new_names.extend(c['name'].split())
-    conflicts = list(filter(lambda c: filter(lambda n: n in new_names,
-                                             c['name'].split()),
-                            db.execute('SELECT * FROM character').fetchall()))
-    if conflicts:
-        print('Potential Conflicts:')
-        for conflict in conflicts:
-            for attr in conflict:
-                print(attr, end='|')
-            print()
-        assert input('Proceed anyway? [y/n] ').lower() == 'y'
+    assert input('Proceed? [y/n] ').lower() == 'y'
 
     # Insert Data
     with db:
-        db.executemany('REPLACE INTO character(id, name, image_url, series, rarity) VALUES(?, ?, ?, ?, ?)',
-                       [(c['id'], c['name'], c['url'], c['series'], c['rarity']) for c in new_chars])
+        db.executemany('REPLACE INTO character(id, name, image_url, series, rarity, batch) VALUES(?, ?, ?, ?, ?, ?)',
+                       [(c['id'], c['name'], c['image_url'], c['series'], c['rarity'], c['batch']) for c in new_chars])
         db.executemany('INSERT OR IGNORE INTO batch(name) VALUES(?)',
                        {(c['batch'],) for c in new_chars})
-        batches = {b['name']: b['id'] for b in db.execute('SELECT name, id FROM batch')}
-        db.executemany('INSERT OR IGNORE INTO character_in_batch(batch, character) VALUES(?, ?)',
-                       [(batches[c['batch']], c['id']) for c in new_chars])
