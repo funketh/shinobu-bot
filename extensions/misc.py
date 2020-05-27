8041e20d6bc0177b2105ff7f70a0eb4334a7d0dc
funketh <theodor.k.funke@gmail.com>
2020-05-27 22:37:54 +0200

    add coin flip command

--- extensions/misc.py
+++ extensions/misc.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 from discord.ext import commands
 
@@ -8,8 +10,16 @@ from api.my_context import Context
 class Misc(commands.Cog):
     @commands.command()
     async def blame(self, ctx: Context, user: discord.User):
-        """Blame someone"""
+        """Blame someone."""
         await ctx.send(f'Blame {user.mention} for everything!')
+
+    @commands.command(aliases=['coin'])
+    async def flip(self, ctx: Context):
+        """Flip a coin."""
+        side_chance = 1/6000
+        result = random.choices(["Heads!", "Tails!", "The coin landed on its side!"],
+                                weights=[.5 - side_chance, .5 - side_chance, side_chance])[0]
+        await ctx.send(result)
 
 
 def setup(bot: shinobu.Shinobu):
