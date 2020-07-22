73d4d03712614d05e3c0dfa7386cd165e5874aba
funketh <theodor.k.funke@gmail.com>
2020-07-22 14:22:03 +0200

    improve logging
    - keep more backups
    - add an additional info.log file

--- utils/setup_logging.py
+++ utils/setup_logging.py
@@ -3,14 +3,19 @@ import logging.handlers
 
 
 def setup_logging():
-    file_handler = logging.handlers.RotatingFileHandler('logs/debug.log', maxBytes=500000, backupCount=7)
-    file_handler.setLevel(logging.DEBUG)
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
+
+    info_file_handler = logging.handlers.RotatingFileHandler('logs/info.log', maxBytes=500000, backupCount=20)
+    info_file_handler.setLevel(logging.INFO)
+
+    debug_file_handler = logging.handlers.RotatingFileHandler('logs/debug.log', maxBytes=500000, backupCount=20)
+    debug_file_handler.setLevel(logging.DEBUG)
+
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s'
                '[%(filename)s:%(funcName)s:%(lineno)d] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S',
         level=logging.DEBUG,
-        handlers=(file_handler, stream_handler)
+        handlers=(debug_file_handler, info_file_handler, stream_handler)
     )
