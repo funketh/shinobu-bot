c56e2c6f42558a12ceaf29b635caeb8e73ac5cf0
funketh <theodor.k.funke@gmail.com>
2020-07-23 00:45:32 +0200

    simplify paginate()

--- utils/formatting.py
+++ utils/formatting.py
@@ -3,9 +3,13 @@ from data.CONSTANTS import MAX_MESSAGE_LENGTH
 
 def paginate(text: str, prefix: str = '', suffix: str = ''):
     max_content_len = MAX_MESSAGE_LENGTH - len(prefix) - len(suffix)
+    text_length = len(text)
     i = 0
-    while i < len(text):
+    while i < text_length:
         max_j = i + max_content_len
-        j = len(text) if max_j > len(text) else i + text[i:max_j].rfind('\n') + 1
+        if max_j > text_length:
+            j = text_length
+        else:
+            j = i + text[i:max_j].rfind('\n') + 1
         yield prefix + text[i:j] + suffix
         i = j
