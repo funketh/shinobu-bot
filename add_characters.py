b090b09cc24369e881fff328c48531aae7faa450
funketh <theodor.k.funke@gmail.com>
2020-08-04 23:05:14 +0200

    improve character insertion script
    should catch invalid data better now

--- add_characters.py
+++ add_characters.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
-import sys
-
 import csv
+import sys
+from typing import NamedTuple
 
 from utils import database
 
 
+Char = NamedTuple('Char', id=int, name=str, image_url=str, series=str, rarity=int, batch=str)
+CHAR_ATTR_ORDER = 'id, name, image_url, series, rarity, batch'
+def throw(): raise ValueError
+
+
 if __name__ == '__main__':
     with open(sys.argv[1], newline='') as tsvfile:
-        new_chars = list(csv.DictReader(tsvfile, delimiter='\t'))
-    db = database.connect()
-    id_key = [k for k in new_chars[0].keys() if k.startswith('id')][0]
-    tmp = []
-    for char in new_chars:
-        char = {k: v.strip() for k, v in char.items()}
-        try:
-            char['id'] = int(char[id_key])
-            del char[id_key]
-        except ValueError:
-            print(f'ERRONEOUS ENTRY: {char["name"]}')
-        else:
-            char['rarity'] = int(char['rarity'])
-            char['image_url'] = char['image_url'] or None
-            tmp.append(char)
-    new_chars = tmp
+        raw_char_rows = list(csv.DictReader(tsvfile, delimiter='\t'))
 
-    assert input('Proceed? [y/n] ').lower() == 'y'
+    # Figure out which column contains the ids
+    id_key_candidates = [k for k in raw_char_rows[0].keys() if k.startswith('id')]
+    if len(id_key_candidates) > 1:
+        raise ValueError('Multiple columns could be the id!')
+    if len(id_key_candidates) == 0:
+        raise ValueError('No column looks like it contains the ids (none of them start with "id")!')
+    id_key = id_key_candidates[0]
+
+    # Sanitize the raw data into proper data types and check its validity
+    chars = []
+    for row in raw_char_rows:
+        try:
+            chars.append(Char(id=int(row[id_key].strip()),
+                              name=row['name'].strip() or throw(),
+                              image_url=row['image_url'].strip() or None,
+                              series=row['series'].strip() or throw(),
+                              rarity=int(row['rarity'].strip()),
+                              batch=row['batch'].strip() or throw()))
+        except Exception as e:
+            raise ValueError(f'ERRONEOUS ENTRY: {row}') from e
 
     # Insert Data
-    with db:
-        db.executemany('REPLACE INTO character(id, name, image_url, series, rarity, batch) VALUES(?, ?, ?, ?, ?, ?)',
-                       [(c['id'], c['name'], c['image_url'], c['series'], c['rarity'], c['batch']) for c in new_chars])
-        db.executemany('INSERT OR IGNORE INTO batch(name) VALUES(?)',
-                       {(c['batch'],) for c in new_chars})
+    with database.connect() as db:
+        db.executemany(f'REPLACE INTO character({CHAR_ATTR_ORDER}) VALUES(?,?,?,?,?,?)', chars)
+        db.executemany('INSERT OR IGNORE INTO batch(name) VALUES(?)', {(c.batch,) for c in chars})
