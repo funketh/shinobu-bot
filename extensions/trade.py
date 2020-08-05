1a3e80c025609639231c16d3b04679a77e7bed6d
funketh <theodor.k.funke@gmail.com>
2020-08-05 18:58:50 +0200

    clean up trade code

--- extensions/trade.py
+++ extensions/trade.py
@@ -5,7 +5,8 @@ import sqlite3
 from abc import abstractmethod
 from collections import defaultdict, UserList
 from contextlib import AsyncExitStack
-from functools import wraps
+from dataclasses import dataclass
+from functools import wraps, partial
 from typing import List, Coroutine, Callable, DefaultDict, Protocol, Set
 
 import discord
@@ -19,8 +20,18 @@ from utils import database
 from utils.database import DB, Waifu
 from utils.waifus import find_waifu, add_money
 
+change_dataclass = partial(dataclass, frozen=True)
 
+
+@change_dataclass
 class Change(Protocol):
+    from_id: int
+    to_id: int
+
+    def __post_init__(self):
+        if self.from_id == self.to_id:
+            raise ExpectedCommandError("You can't give something to yourself!")
+
     @abstractmethod
     async def execute(self, db: DB): ...
 
@@ -28,41 +39,34 @@ class Change(Protocol):
     def __str__(self): ...
 
 
+@change_dataclass
 class WaifuTransfer(Change):
-    def __init__(self, waifu: Waifu, old_owner_id: int, new_owner_id: int):
-        if old_owner_id == new_owner_id:
-            raise ExpectedCommandError("You can't give something to yourself!")
-        self.waifu = waifu
-        self.old_owner_id = old_owner_id
-        self.new_owner_id = new_owner_id
+    waifu: Waifu
 
     async def execute(self, db: DB):
         try:
-            db.execute('UPDATE waifu SET user=? WHERE id=?', [self.new_owner_id, self.waifu.id])
+            db.execute('UPDATE waifu SET user=? WHERE id=?', [self.to_id, self.waifu.id])
         except sqlite3.IntegrityError:
             raise ExpectedCommandError("You can't give someone a waifu they already own!")
 
     def __str__(self):
-        return (
-            f"<@{self.old_owner_id}> gives"
-            f" ***{self.waifu.rarity.name}*** **{self.waifu.character.name}**"
-            f" [{self.waifu.character.series}] to <@{self.new_owner_id}>"
-        )
+        return (f"<@{self.from_id}> gives"
+                f" ***{self.waifu.rarity.name}*** **{self.waifu.character.name}**"
+                f" [{self.waifu.character.series}] to <@{self.to_id}>")
 
 
+@change_dataclass
 class MoneyTransfer(Change):
-    def __init__(self, amount: int, from_id: int, to_id: int):
-        if from_id == to_id:
-            raise ExpectedCommandError("You can't give something to yourself!")
-        if amount <= 0:
+    amount: int
+
+    def __post_init__(self):
+        if self.amount <= 0:
             raise ExpectedCommandError(f"You can only transfer positive amounts of {CURRENCY}!")
-        self.amount = amount
-        self.from_id = from_id
-        self.to_id = to_id
 
     async def execute(self, db: DB):
-        add_money(db, self.from_id, -self.amount)
-        add_money(db, self.to_id, self.amount)
+        with db:
+            add_money(db, self.from_id, -self.amount)
+            add_money(db, self.to_id, self.amount)
 
     def __str__(self):
         return f"<@{self.from_id}> gives {self.amount} {CURRENCY} to <@{self.to_id}>"
@@ -70,6 +74,7 @@ class MoneyTransfer(Change):
 
 class LockedList(UserList):
     def __init__(self, initlist=None):
+        # TODO: enforce locking
         super().__init__(initlist)
         self.lock = asyncio.Lock()
 
@@ -80,6 +85,7 @@ _CHANGES: DefaultDict[discord.User, LockedList[Change]] = defaultdict(LockedList
 def forbid(func: Callable[..., Coroutine]):
     @wraps(func)
     async def wrapper(*args, **kwargs):
+        # TODO: use the appropriate locks in this and in @require
         ctx = args[0] if isinstance(args[0], Context) else args[1]
         if not _CHANGES[ctx.author]:
             await func(*args, **kwargs)
@@ -121,7 +127,7 @@ class Trade(commands.Cog):
         """Give one of your waifus to the specified user"""
         db = database.connect()
         waifu = find_waifu(db, ctx.author.id, ' '.join(search_terms))
-        transfer = WaifuTransfer(waifu=waifu, old_owner_id=ctx.author.id, new_owner_id=partner.id)
+        transfer = WaifuTransfer(from_id=ctx.author.id, to_id=partner.id, waifu=waifu)
         change_list = _CHANGES[ctx.author]
         async with change_list.lock:
             change_list.append(transfer)
@@ -130,7 +136,7 @@ class Trade(commands.Cog):
     @trade.command(name='money', aliases=['m'])
     async def trade_money(self, ctx: Context, partner: discord.User, amount: int):
         """Give money to someone"""
-        transfer = MoneyTransfer(amount=amount, from_id=ctx.author.id, to_id=partner.id)
+        transfer = MoneyTransfer(from_id=ctx.author.id, to_id=partner.id, amount=amount)
         change_list = _CHANGES[ctx.author]
         async with change_list.lock:
             change_list.append(transfer)
@@ -146,8 +152,8 @@ class Trade(commands.Cog):
                 await stack.enter_async_context(_CHANGES[s].lock)
             all_changes: List[Change] = [c for s in signers for c in _CHANGES[s]]
             changes_str = '\n'.join(str(c) for c in all_changes)
-            msg = await ctx.send(f"{', '.join(s.mention for s in signers)}: "
-                                 f"Do you accept the following changes?\n{changes_str}")
+            mention_str = ', '.join(s.mention for s in signers)
+            msg = await ctx.send(f"{mention_str}: Do you accept the following changes?\n{changes_str}")
             if await ctx.confirm_multiuser(msg, users=signers):
                 with database.connect() as db:
                     for c in all_changes:
@@ -156,7 +162,7 @@ class Trade(commands.Cog):
                         _CHANGES[s].clear()
                     await ctx.info("Successfully executed transaction.")
             else:
-                return await ctx.error("Cancelled execution! (Transaction contents are kept)")
+                raise ExpectedCommandError("Cancelled execution! (Transaction contents are kept)")
 
 
 def setup(bot: Shinobu):
